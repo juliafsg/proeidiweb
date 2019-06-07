73aa100e279168851ebfff6570e03e98025d4355
Julia Ferreira de Souza Gloria <juliafsg@A307-36L.imd.local>
2019-06-07 18:02:07 -0300

    Organização + Primeiras classes

---
+++ Desenvolvimento/src/entidades/Frequencia.java
@@ -0,0 +1,50 @@
+package entidades;
+
+import java.sql.Date;
+
+public class Frequencia {
+	private int idPessoa;
+	private int idTurma;
+	private Date data;
+	private boolean estavaFrequente;
+	
+	public Frequencia(int idPessoa, int idTurma, Date data, boolean estavaFrequente) {
+		super();
+		this.idPessoa = idPessoa;
+		this.idTurma = idTurma;
+		this.data = data;
+		this.estavaFrequente = estavaFrequente;
+	}
+
+	public int getIdPessoa() {
+		return idPessoa;
+	}
+
+	public void setIdPessoa(int idPessoa) {
+		this.idPessoa = idPessoa;
+	}
+
+	public int getIdTurma() {
+		return idTurma;
+	}
+
+	public void setIdTurma(int idTurma) {
+		this.idTurma = idTurma;
+	}
+
+	public Date getData() {
+		return data;
+	}
+
+	public void setData(Date data) {
+		this.data = data;
+	}
+
+	public boolean isEstavaFrequente() {
+		return estavaFrequente;
+	}
+
+	public void setEstavaFrequente(boolean estavaFrequente) {
+		this.estavaFrequente = estavaFrequente;
+	}
+}
--- Desenvolvimento/src/entidades/Turma.java
+++ Desenvolvimento/src/entidades/Turma.java
@@ -9,5 +9,45 @@ public class Turma{
     private int idCurso;
     private Boolean consolidada;
     
+	public Turma(int id, int vagas, int idSala, int idCurso, Boolean consolidada) {
+		super();
+		this.id = id;
+		this.vagas = vagas;
+		this.idSala = idSala;
+		this.idCurso = idCurso;
+		this.consolidada = consolidada;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getVagas() {
+		return vagas;
+	}
+	public void setVagas(int vagas) {
+		this.vagas = vagas;
+	}
+	public int getIdSala() {
+		return idSala;
+	}
+	public void setIdSala(int idSala) {
+		this.idSala = idSala;
+	}
+	public int getIdCurso() {
+		return idCurso;
+	}
+	public void setIdCurso(int idCurso) {
+		this.idCurso = idCurso;
+	}
+	public Boolean getConsolidada() {
+		return consolidada;
+	}
+	public void setConsolidada(Boolean consolidada) {
+		this.consolidada = consolidada;
+	}
+    
    
 }
